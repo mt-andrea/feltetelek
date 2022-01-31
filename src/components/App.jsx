f45fe19f4ed320d163a67cbe8acf9494546c2d88
mt-andrea <mateandicica@gmail.com>
2022-01-31 18:41:26 +0100

    login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,7 @@ function App() {
   return (
     <div className="container">
       <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      
     </div>
   );
 }
